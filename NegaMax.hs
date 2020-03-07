180df288f4e25d4ca6336132ffbadffdd638f0a0
Jake Wood <jakew@captricity.com>
2020-03-06 23:14:44 -0800

    negamax working

--- NegaMax.hs
+++ NegaMax.hs
@@ -1,55 +1,60 @@
 module NegaMax where
 
-import qualified Data.List as L
+import Data.List (genericTake, genericDrop, maximumBy)
 
 data Player = X | O
-data Cell = Player | Empty
-type Board = [Player]
+  deriving (Show, Read, Eq)
+type Cell = Maybe Player
+type Board = [Cell]
 
-
-invert :: Player -> Player
-invert X = O
-invert O = X
+alternate :: Player -> Player
+alternate X = O
+alternate O = X
 
 score :: Board -> Player -> Integer
 score (c0:c1:c2:[]) p1
-  | winner && c1 == p1 = 1
-  | winner && c1 == p2 = -1
+  | winner && c1 == Just p1 = 1
+  | winner && c1 == Just p2 = -1
   | otherwise = 0
   where
-    p2 = invert p1
-    winner =  c0 == c1 || c1 == c2 && c1 == p1
+    p2 = alternate p1
+    winner =  c0 == c1 || c1 == c2
 
 terminal :: Board -> Bool
-terminal board = score board O /= 0 || find (== Empty) == Nothing
+terminal board = score board X /= 0 || (not $ elem Nothing board)
 
 enumerate :: [a] -> [(Integer, a)]
 enumerate = zip [0..]
 
 insertAt :: [a] -> a -> Integer -> [a]
-insert i _ _ | i < 0 = error "Cannot insert into list with negative index."
-insertAt i e es = L.genericTake i es ++ e : L.genericDrop (i + 1) es
+insert es _ i | i < 0 = es
+insertAt es e i = genericTake i es ++ e : genericDrop (i + 1) es
 
+-- VERIFIED
 allMoves :: Board -> Player -> [Board]
-allMoves board player = map (insertAt board player) openMoves
+allMoves board player = map (insertAt board $ Just player) openMoves
   where
-    openMoves = map fst $ filter ((== Empty) . snd) (enumerate board)
+    openMoves = map fst $ filter ((== Nothing) . snd) (enumerate board)
 
-maximumBySnd :: (Ord b) => f (a, b) -> (a, b)
-maximumBySnd = L.maximumBy (\(_, s1) (_, s2) -> compare s1 s2)
+-- VERIFIED
+maximumBySnd :: (Ord b, Foldable f) => f (a, b) -> (a, b)
+maximumBySnd = maximumBy (\(_, s1) (_, s2) -> compare s1 s2)
 
-negamax :: Board -> Player -> (Board, Integer)
-negamax board player
-  | terminal board = score board player
-  | otherwise = (\(a, i) -> (a, -i)) bestMove  -- negates score
+
+negamax :: Player -> Board -> (Board, Integer)
+negamax player board
+    | terminal board = ([], score board player)
+    | otherwise = maximumBySnd $ zip moves values
   where
     moves = allMoves board player
-    bestMove = maximumBySnd $ map (negamax $ invert player) moves
+    values = map (negate . snd . (negamax $ alternate player)) moves
 
-
-startBoard = [Empty, Empty, Empty]
-
+startBoard = [Nothing, Nothing, Nothing]
+sndBoard = [Nothing, Just X, Nothing]
+trdBoard = [Nothing, Just X, Just O]
 
 main :: IO ()
 main = do
-  putStr . show $ negamax startBoard O
+  putStr . show $ negamax X startBoard
+  putStr . show $ negamax O sndBoard
+  putStr . show $ negamax X trdBoard
